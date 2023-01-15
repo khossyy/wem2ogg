bc4e2757bbac33a3fcdf67f09b501de7f5eebd04
khossyy <107783076+khossyy@users.noreply.github.com>
2023-01-15 15:27:23 +0800

    updated convert.bat
    coping this works

--- convert.bat
+++ convert.bat
@@ -1,4 +1,4 @@
 for %%f in (*.wem) do "./ww2ogg024/ww2ogg.exe" %%f --pcb "./ww2ogg024/packed_codebooks_aoTuV_603.bin"
 pause
-for %%f in (*.ogg) do ReVorb.exe %%f
+for %%f in (*.ogg) do ReVorb.exe %%f %%f
 pause
